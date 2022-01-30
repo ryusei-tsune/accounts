cff5f28ac6333c25c6680dfecbe87a49351a0793
ryusei-tsune <rt1226.tennis@gmail.com>
2022-01-30 16:04:37 +0900

    adjust Mongodb property

--- routes/api/process.js
+++ routes/api/process.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
 
 //const MongoClient = MongoDb.MongoClient
 
@@ -13,18 +15,20 @@ router.get("/acquisition/:id", async (req, res, next) => {
       useUnifiedTopology: true,
     });
 
-    client.connect((err) => {
+    client.connect(async (err) => {
       const db = client.db(process.env.DB);
       const collection = db.collection("expense");
       const user = { username: req.params.id };
-      const data = collection.find(user);
-      client.close();
+      const data = await collection.estimatedDocumentCount();
       console.log(data);
+      client.close();
+      res.json({ ok: true });
+      //   console.log(data);
     });
   } catch (err) {
     console.log(err?.message);
   }
-  res.send("respond process");
+  //res.send("respond process");
 });
 
 module.exports = router;
