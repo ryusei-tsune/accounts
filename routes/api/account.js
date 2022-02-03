fe0732bbdd5f9e7bec6fe557e7990fcbbb4af53a
ryusei-tsune <rt1226.tennis@gmail.com>
2022-02-03 16:45:09 +0900

    add creating-user-process

--- routes/api/account.js
+++ routes/api/account.js
@@ -1,9 +1,54 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-//import mongodb from 'mongodb'
+//const router = express();
 
-router.get('/', function (req, res, next) {
-    res.send('respond account');
+require("dotenv").config();
+const MongoDB = require("mongodb");
+const MongoClient = MongoDB.MongoClient;
+const mongouri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.MONGOHOST}`;
+const client = new MongoClient(mongouri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const crypto = require("crypto");
+
+function hashed(password) {
+  let result;
+  try {
+    let hash = crypto.createHmac("sha512", password);
+    hash.update(password);
+    result = hash.digest("hex");
+  } catch (err) {
+    result = null;
+  }
+  return result;
+}
+
+router.post("/user", async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
+    await client.connect();
+    const db = client.db(process.env.DB);
+
+    const collection = db.collection("users");
+    const resister = { name: username, password: hashed(password) }; // 保存対象
+    if (!resister.password) {
+      res("Error in password hashing");
+    }
+    const user = { name: { $eq: username } };
+    const existing = await collection.findOne(user);
+    if (existing) {
+      //ダメな時
+      res.json({ existing: true });
+    } else {
+      await collection.insertOne(resister);
+      res.json({ existing: false });
+    }
+  } catch (err) {
+    console.log(err?.message);
+    res.json({ err: true });
+  }
 });
 
-module.exports = router
+module.exports = router;
