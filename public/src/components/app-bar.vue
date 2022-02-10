177a6db16f5f3c1bbf0d205ad29796d98f6c7118
ryusei-tsune <rt1226.tennis@gmail.com>
2022-02-10 16:29:49 +0900

    install vuex

--- public/src/components/app-bar.vue
+++ public/src/components/app-bar.vue
@@ -1,12 +1,33 @@
 <template>
-  <v-app-bar fixed app style="background: cyan">
+  <v-app-bar style="background: cyan">
     <v-toolbar-title>家計簿アプリ</v-toolbar-title>
     <v-spacer></v-spacer>
-    <div>
+    <v-btn small class="ml-2">in</v-btn>
+    <div v-if="userAuth">
       <!-- {{ username }} -->
-      <v-btn small to="/" class="ml-2">
+      <v-btn small to="/" class="ml-2" @click="logout()">
         <v-icon>mdi-logout</v-icon>
       </v-btn>
     </div>
   </v-app-bar>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    userAuth() {
+      return this.$store.state.auth;
+    },
+    username() {
+      return this.$store.state.username;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.commit("userAuthentication", false);
+    },
+  },
+};
+</script>
--- public/src/store/index.js
+++ public/src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    auth: false,
+    username: "",
+  },
+  mutations: {
+    userAuthentication(state, payload) {
+      state.auth = payload;
+      if (!state.auth) {
+        state.username = "";
+      }
+    },
+  },
   actions: {},
   modules: {},
 });
