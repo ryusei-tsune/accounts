8f6209260632bc739af2a7814c07e54bd502423a
ryusei-tsune <rt1226.tennis@gmail.com>
2022-02-15 12:13:48 +0900

    create tab component

--- public/src/views/history.vue
+++ public/src/views/history.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <h1>history</h1>
+    <Tabs>
+      <h1>history</h1>
+    </Tabs>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Tabs from "../components/tabs.vue";
+export default {
+  name: "History",
+  components: {
+    Tabs,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
--- public/src/views/resister.vue
+++ public/src/views/resister.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <h1>resister</h1>
+    <Tabs>
+      <h1>resister</h1>
+    </Tabs>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import Tabs from "../components/tabs.vue";
+export default {
+  name: "Resiter",
+  components: {
+    Tabs,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
