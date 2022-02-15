64bc394c1a5308794fd20341fabe676684fddc65
ryusei-tsune <rt1226.tennis@gmail.com>
2022-02-16 00:03:27 +0900

    update resister page

--- public/src/views/resister.vue
+++ public/src/views/resister.vue
@@ -1,20 +1,52 @@
 <template>
   <div>
     <Tabs>
-      <h1>resister</h1>
+      <v-row justify="center" class="mt-2">
+        <resisterItem type="expense">
+          <template v-slot:type>
+            <v-card-text>支出</v-card-text>
+          </template>
+          <template v-slot:example>
+            例：
+            <v-spacer></v-spacer>
+            2022/01/01
+            <v-spacer></v-spacer>
+            食費
+            <v-spacer></v-spacer>
+            300
+          </template>
+        </resisterItem>
+        <resisterItem type="income">
+          <template v-slot:type>
+            <v-card-text>収入</v-card-text>
+          </template>
+          <template v-slot:example>
+            例：
+            <v-spacer></v-spacer>
+            2022/01/25
+            <v-spacer></v-spacer>
+            給料
+            <v-spacer></v-spacer>
+            {{ num.toLocaleString() }}
+          </template>
+        </resisterItem>
+      </v-row>
     </Tabs>
   </div>
 </template>
 <script>
-// @ is an alias to /src
 import Tabs from "../components/tabs.vue";
+import resisterItem from "../components/resister/resisterItem.vue";
 export default {
   name: "Resiter",
   components: {
     Tabs,
+    resisterItem,
   },
   data() {
-    return {};
+    return {
+      num: 50000,
+    };
   },
   mounted() {},
   methods: {},
